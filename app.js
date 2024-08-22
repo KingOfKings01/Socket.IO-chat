f3794fd733038d4e34ec6e8212b7b524bbbb68d9
KingOfKings01 <aasifkhan9605@gmail.com>
2024-08-22 18:34:03 +0530

    socket.io connextion set. create index.html and serve it at root url.

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+app.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
